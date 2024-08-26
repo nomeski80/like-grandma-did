9d97259d004517667b5bb4f9057e88bcf8699abf
Naomi Hancock <naomi_hancock@yahoo.co.uk>
2024-08-26 12:41:40 +0000

    edited admin to support summernote to show gui

--- recipes/admin.py
+++ recipes/admin.py
@@ -3,13 +3,13 @@ from .models import Recipe, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Recipe)
-class PostAdmin(SummernoteModelAdmin):
+class RecipesAdmin(SummernoteModelAdmin):
 
-    list_display = ('name_of_recipe', 'slug', 'status' , 'created_on')
+    list_display = ('name_of_recipe', 'slug', 'status')
     search_fields = ['name_of_recipe']
     list_filter = ('status',)
     prepopulated_fields = {'slug': ('name_of_recipe',)}
-    summernote_fields = ('content',)
+    summernote_fields = ('ingredients', 'instructions')
 
 # Register your models here.
 
