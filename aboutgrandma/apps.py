e82265f136685cc1cdad44d9d9f27f1b7d4a5060
Naomi Hancock <naomi_hancock@yahoo.co.uk>
2024-08-31 13:31:21 +0000

    added a view and post detail file, added grandma logo

---
+++ aboutgrandma/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AboutgrandmaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'aboutgrandma'
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Recipe
 
@@ -7,3 +7,21 @@ class PostList(generic.ListView):
     queryset = Recipe.objects.all()
     template_name = "recipes/index.html"
     paginate_by = 6
+
+def post_detail(request, slug):
+        """
+    Display an individual :model:`recipe.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`recipe.Post`.
+
+    **Template:**
+
+    :template:`recipe/post_detail.html`
+    """
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        return render(request, "recipes/post_detail.html", {"recipes":Recipe },)
