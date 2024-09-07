8dbd31c8ae5cbac1bef3a29bdc4af5acfb348d6d
Naomi Hancock <naomi_hancock@yahoo.co.uk>
2024-09-07 12:57:08 +0000

    added comments form to enable CRUD funcationality

--- like_grandma_did/settings.py
+++ like_grandma_did/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-nomeski80-likegrandmadi-gegor6whfae.ws.codeinstitute-ide.net' ,'.herokuapp.com' ]
 
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'hello_world', 
     'recipes',
     'django_summernote',
@@ -55,6 +57,9 @@ INSTALLED_APPS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
