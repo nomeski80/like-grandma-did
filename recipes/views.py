505097d0dd93fbbf63869dc2426e2bca2f0c52b2
Naomi Hancock <naomi_hancock@yahoo.co.uk>
2024-09-07 19:28:10 +0000

    added comment and shows a message to show a comment has been submitted

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Recipe
 from .forms import CommentForm
 
@@ -10,31 +11,44 @@ class PostList(generic.ListView):
     paginate_by = 6
 
 def post_detail(request, slug):
-        """
-    Display an individual :model:`recipe.Post`.
+    """
+    Display an individual :model:`blog.Post`.
 
     **Context**
 
     ``post``
-        An instance of :model:`recipe.Post`.
+        An instance of :model:`blog.Post`.
 
     **Template:**
 
-    :template:`recipe/post_detail.html`
+    :template:`blog/post_detail.html`
     """
-
-        queryset = Recipe.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.all().order_by("-created_on")
-        comment_count = post.comments.filter(approved=True).count()
-        comment_form = CommentForm()
-
-        return render(request, 
-        "recipes/post_detail.html", 
+    queryset = Recipe.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    
+    comment_count = post.comments.filter(approved=True).count()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.body = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+    )
+            
+    comment_form = CommentForm()
+    
+    return render(
+        request,
+        "recipes/post_detail.html",
         {
-        "recipe": post,
-        "comments": comments,
-        "comment_count": comment_count,
-        "comment_form": comment_form, 
+            "recipe": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form
         },
-        )
+    )
